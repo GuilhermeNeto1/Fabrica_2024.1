0f1c276ceb2355788dd6bf777f0681b886fec971
GuilhermeNeto1 <48604260+GuilhermeNeto1@users.noreply.github.com>
2024-03-04 17:36:17 -0300

    fiz as atividades doa dia1

---
+++ Desafio2.py
@@ -0,0 +1,12 @@
+inteiro1 = int(input("Digite o valor inteiro: "))
+inteiro2 = int(input("Digite o valor inteiro: "))
+soma = inteiro1 + inteiro2
+print(soma)
+
+float1 = float(input("Digite o valor float: "))
+float2 = float(input("Digite o valor float: "))
+
+subtracao = float1 - float2 
+print(subtracao)
+
+
---
+++ desafio1.py
@@ -0,0 +1,11 @@
+dicionario = {}
+string = dicionario["string"] = "string"
+inteiro = dicionario["inteiro"] = 10
+boleano = dicionario["bolean"] = True
+decimal = dicionario["float"] = 0.10
+print(decimal)
+print(boleano)
+
+lista = ["PB","PE","PI"]
+print(lista[0])
+
